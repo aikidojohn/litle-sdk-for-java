2d9656128c22fec4eb370ad69495f213e02d301a
Ting Wang - Engineering <twang@twang-vm1.dwi.litle.com>
2014-08-04 16:33:29 -0400

    add batch and Rfr

---
+++ samples/other/src/main/java/RfrLitleExample.java
@@ -0,0 +1,23 @@
+package com.litle.sdk.samples;
+import com.litle.sdk.*;
+import com.litle.sdk.generate.AccountUpdateFileRequestData;
+import com.litle.sdk.generate.RFRRequest;
+import java.util.Calendar;
+public class RfrLitleExample {
+    public static void main(String[] args) {
+        String merchantId = "0180";
+        String requestFileName = "litleSdk-testRFRFile-fileConfigSFTP.xml";
+        RFRRequest rfrRequest = new RFRRequest();
+        AccountUpdateFileRequestData data = new AccountUpdateFileRequestData();
+        data.setMerchantId(merchantId);
+        data.setPostDay(Calendar.getInstance());
+        rfrRequest.setAccountUpdateFileRequestData(data);
+         
+        LitleRFRFileRequest request = new LitleRFRFileRequest(requestFileName, rfrRequest);
+         
+        LitleRFRFileResponse response = request.sendToLitleSFTP();
+         
+        System.out.println("Response: " + response.getMessage());
+
+    }
+}
